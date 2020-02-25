4359ee1d6197dcb980632a61f1d4dc5814f8bf3e
Stepan-Kolarovsky <stepan.kolarovsky@student.ossp.cz>
2020-02-25 08:39:28 +0100

    Create index.php
    přidán index a 6 proměních

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+   $a=10;
+   $b=5;
+?> 
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+ <p>a = <?= $a ?> </p>
+ <p>b = <?= $b ?> </p>
+ <p>Součet: <?= $a ?> + <?= $b ?> = <?= $a + $b ?> </p>
+ <p>Rozdíl: <?= $a ?> - <?= $b ?> = <?= $a - $b ?> </p>
+ <p>Součin: <?= $a ?> × <?= $b ?> = <?= $a * $b ?> </p>
+ <p>Podíl: <?= $a ?> ÷ <?= $b ?> = <?= $a / $b ?> </p>
+</body>
+</html>
